510b22a965931856baa56ea9ff8f5f8e4ad61040
Kangkan <geekays@gmail.com>
2017-11-21 17:44:49 +0530

    Added controllers

---
+++ Converter/Controllers/CtoFController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Converter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/CtoF")]
+    public class CtoFController : Controller
+    {
+        // GET: api/CtoF
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "This is a converter from C to F" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{c}")]
+        public string Get(int c)
+        {
+            return (((9 * c) / 5) + 32).ToString();
+        }
+
+    }
+}
---
+++ Converter/Controllers/FtoCController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Converter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/FtoC")]
+    public class FtoCController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "This is a converter for F to C" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{f}")]
+        public string Get(int f)
+        {
+            return ((f - 32) * 5 / 9).ToString();
+        }
+    }
+}
